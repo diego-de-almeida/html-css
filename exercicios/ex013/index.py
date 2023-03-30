40bcfa2295783a99be1bbc3c042bffe1008cc130
Diego de Almeida <diegodealmeida16@gmail.com>
2023-03-30 12:47:33 -0300

    Initial commit

---
+++ exercicios/ex013/index.py
@@ -0,0 +1,18 @@
+# Definir uma função para gerar a sequência de Fibonacci
+def fibonacci(n):
+  # Inicializar as duas primeiras variáveis da sequência
+  a = 0
+  b = 1
+  # Criar uma lista vazia para armazenar os termos da sequência
+  lista = []
+  # Usar um laço for para iterar n vezes
+  for i in range(n):
+    # Adicionar o valor de a à lista
+    lista.append(a)
+    # Atualizar os valores de a e b usando a regra de Fibonacci
+    a, b = b, a + b
+  # Retornar a lista com os termos da sequência
+  return lista
+
+# Testar a função com um exemplo
+print(fibonacci(10))
